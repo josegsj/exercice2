a919c02deb7ff75b34c800fe7fbe03fdda313697
josegsj <josegrasul@gmail.com>
2019-08-18 19:55:46 -0300

    Beans de pauta e votação com anotaçoes de persistencia

---
+++ Exercice/src/main/java/br/exercice/bean/Ruling.java
@@ -0,0 +1,36 @@
+package br.exercice.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Ruling")
+public class Ruling {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	@Column(name="name",nullable=true,length=100,updatable=true)
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
--- Exercice/src/main/java/br/exercice/bean/Vote.java
+++ Exercice/src/main/java/br/exercice/bean/Vote.java
@@ -1,8 +1,50 @@
 package br.exercice.bean;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="vote")
 public class Vote {
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	@Column(name="vote",nullable=true,length=100,updatable=true)
+	private boolean vote;
+	@Column(name="cpf",nullable=true,length=100,updatable=true)
+	private String cpf;
+	@Column(name="session_id",nullable=true,length=100,updatable=true)
+	private int sessionID;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean isVote() {
+		return vote;
+	}
+	public void setVote(boolean vote) {
+		this.vote = vote;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getSessionID() {
+		return sessionID;
+	}
+	public void setSessionID(int sessionID) {
+		this.sessionID = sessionID;
+	} 
+	
+	
 
 }
